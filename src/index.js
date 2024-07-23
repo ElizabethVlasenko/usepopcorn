3e8bc87fa74300feafa791c44ed588583dc2397c
ElizabethVlasenko <elizabeth.wolker@gmail.com>
2024-07-23 12:20:22 +0100

    Added a error handlers on the fetchMovies function

--- src/index.js
+++ src/index.js
@@ -1,30 +1,11 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import StarRating from "./StarRating";
-//import "./index.css";
-//import App from "./App";
-function Test() {
-  const [movieRating, setMovieState] = useState(0);
+import "./index.css";
+import App from "./App";
 
-  function setMovieRating(rating) {
-    setMovieState(rating);
-  }
-  return (
-    <div>
-      <StarRating color="blue" onSetRating={setMovieRating}></StarRating>
-      <p>This movie was rated {movieRating} stars</p>
-    </div>
-  );
-}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
-      MaxRating={5}
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-      defaultRating={3}
-    />
-    <Test></Test>
+    <App />
   </React.StrictMode>
 );
